64c9d05cff0a2b99ab2159262ea4199aea12fdbe
Amanda Lacanna <amanda.lacanna@gmail.com>
2018-03-04 14:38:02 -0300

    Set apiKey in request

--- NewsApp/api/src/main/java/com/amandalacanna/api/request/NewsRequest.kt
+++ NewsApp/api/src/main/java/com/amandalacanna/api/request/NewsRequest.kt
@@ -1,14 +1,15 @@
 package com.amandalacanna.api.request
 
+import com.amandalacanna.api.BuildConfig
 import com.amandalacanna.api.response.NewsResponse
 import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-
 interface NewsRequest {
     @GET("v2/everything?q=URL-encoded")
     fun getNews (@Query("page") page: Int,
-                      @Query("pageSize") pageSize: Int): Observable<NewsResponse>
+                 @Query("pageSize") pageSize: Int,
+                 @Query("apiKey") apiKey: String = BuildConfig.NEWS_API_KEY): Observable<NewsResponse>
 
 }
