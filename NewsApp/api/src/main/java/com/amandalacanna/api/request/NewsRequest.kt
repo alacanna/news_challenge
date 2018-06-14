de9ba3dad8a0a69c9532b4bdf0a504a26c59b9ea
Amanda Lacanna <amanda.lacanna@gmail.com>
2018-06-13 23:59:53 -0300

    Atualiza dependencias

--- NewsApp/api/src/main/java/com/amandalacanna/api/request/NewsRequest.kt
+++ NewsApp/api/src/main/java/com/amandalacanna/api/request/NewsRequest.kt
@@ -3,13 +3,13 @@ package com.amandalacanna.api.request
 import com.amandalacanna.api.BuildConfig
 import com.amandalacanna.api.response.NewsResponse
 import io.reactivex.Observable
-import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface NewsRequest {
+
     @GET("v2/everything?q=URL-encoded")
     fun getNews (@Query("page") page: Int,
                  @Query("pageSize") pageSize: Int,
                  @Query("apiKey") apiKey: String = BuildConfig.NEWS_API_KEY): Observable<NewsResponse>
-
+    
 }
